f3414276e4ed0511e36d36c01b7882427a35be78
Krool <33756666+Kr00l@users.noreply.github.com>
2020-02-05 00:18:26 +0100

    Delete replace_cobj.bat

--- Src/replace_cobj.bat
+++
@@ -1,4 +0,0 @@
-del ..\Bin\sqlite3win32helper.obj
-del ..\Bin\sqlite3win32stubs.obj
-copy ..\Bin\sqlite3win32helper.cobj ..\Bin\sqlite3win32helper.obj
-copy ..\Bin\sqlite3win32stubs.cobj ..\Bin\sqlite3win32stubs.obj

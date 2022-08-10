06a7652e1a6f2f082a8df9706c1e4ef75fd20a4f
Krool <33756666+Kr00l@users.noreply.github.com>
2022-08-10 21:57:00 +0200

    Add files via upload

--- Lib/build.bat
+++ Lib/build.bat
@@ -1,14 +1,16 @@
 @echo off
 setlocal
-set cl_exe="C:\Program Files (x86)\Microsoft Visual Studio\VC98\Bin\cl.exe" /nologo /MD /Ot
+set cl_exe="C:\Program Files (x86)\Microsoft Visual Studio\VC98\Bin\cl.exe" /nologo /MD /O2 /Og- /Gz
 set bin_dir=..\Bin
+set bin_file=sqlite3win32.dll
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
 
 pushd %~dp0
 
-%cl_exe% /LD sqlite3win32stubs.cpp sqlite3win32helper.c /Fesqlite3win32.dll /link /DEF:sqlite3win32.def
-copy sqlite3win32.dll %bin_dir% > nul
-copy sqlite3win32stubs.obj %bin_dir%\*.cobj > nul
-copy sqlite3win32helper.obj %bin_dir%\*.cobj > nul
+%cl_exe% /LD sqlite3win32stubs.cpp sqlite3win32helper.c /Fe%bin_file% /link /DEF:sqlite3win32.def
+copy %bin_file% %bin_dir% > nul
+copy *.obj %bin_dir%\*.cobj > nul
 
 :cleanup
 del /q *.exp *.lib *.obj *.dll ~$*
--- Lib/build_v140_xp.bat
+++ Lib/build_v140_xp.bat
@@ -2,8 +2,8 @@
 setlocal
 set cl_exe="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\cl.exe" /nologo /MT /O2 /Gz
 :: /arch:IA32
-
 set bin_dir=..\Bin
+set bin_file=sqlite3win32.dll
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 
@@ -12,10 +12,9 @@ set "lib=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\lib;%lib%"
 
 pushd %~dp0
 
-%cl_exe% /LD sqlite3win32stubs.cpp sqlite3win32helper.c /Fesqlite3win32.dll /link /DEF:sqlite3win32.def
-copy sqlite3win32.dll %bin_dir% > nul
-copy sqlite3win32stubs.obj %bin_dir%\*.cobj > nul
-copy sqlite3win32helper.obj %bin_dir%\*.cobj > nul
+%cl_exe% /LD sqlite3win32stubs.cpp sqlite3win32helper.c /Fe%bin_file% /link /DEF:sqlite3win32.def
+copy %bin_file% %bin_dir% > nul
+copy *.obj %bin_dir%\*.cobj > nul
 
 :cleanup
 del /q *.exp *.lib *.obj *.dll ~$*
